8fde33acc85bb24448a851438094c3724985ad44
kareemabdullah55 <kareem.abuullah@gmail.com>
2025-08-31 04:19:13 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+// Menu toggle for mobile
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
